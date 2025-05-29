bac7eb83a5293d638ed58737bd63d4fbd0aff066
Juan Castro <juanp.castrom@unac.edu.co>
2025-05-29 05:56:22 -0500

    💥 New commit develoment component festivo

--- festivosDias/src/app/app.config.ts
+++ festivosDias/src/app/app.config.ts
@@ -4,11 +4,10 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
-
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }), 
-    provideRouter(routes), 
-    provideHttpClient(withInterceptorsFromDi()),]
+    provideRouter(routes),
+    provideHttpClient(withInterceptorsFromDi())
+  ]
 };
--- festivosDias/src/features/componentes/festivo/festivo.component.ts
+++ festivosDias/src/features/componentes/festivo/festivo.component.ts
@@ -1,16 +1,31 @@
 import { Component } from '@angular/core';
 import { ReferenciasMaterialModule } from '../../../shared/modulos/referencias-material.module';
 import { NgxDatatableModule } from '@swimlane/ngx-datatable';
+import { Festivo } from '../../../shared/entidades/festivo';
+import { FestivoService } from '../../../core/servicios/festivo.service';
 
 @Component({
   selector: 'app-festivo',
   imports: [
     ReferenciasMaterialModule,
-        NgxDatatableModule,
+    NgxDatatableModule,
   ],
   templateUrl: './festivo.component.html',
   styleUrl: './festivo.component.css'
 })
 export class FestivoComponent {
+  public columnas = [
+    { name: "Nombre del festivo", prop: "nombre" },
+    { name: "Dia del festivo", prop: "dia" },
+    { name: "Mes del festivo", prop: "mes" },
+    { name: "Pais del festivo", prop: "pais" },
+    { name: "Tipo del festivo", prop: "tipo" }
+  ];
+
+    public festivo: Festivo[] = [];
+
+    constructor(private servicioFestivo: FestivoService) {
+    }
 
 }
+
