d5d8019e7b0b2a65733f1490bc5a46fb84a8dfa0
Juan Castro <juanp.castrom@unac.edu.co>
2025-05-28 19:05:08 -0500

    💥 New commit add components

--- festivosDias/src/app/app.routes.ts
+++ festivosDias/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { InicioComponent } from '../features/componentes/inicio/inicio.component';
+import { SeleccionComponent } from '../features/componentes/seleccion/seleccion.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "inicio", component: InicioComponent },
+    { path: "selecciones", component: SeleccionComponent }
+];
---
+++ festivosDias/src/features/componentes/seleccion/seleccion.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-seleccion',
+  imports: [],
+  templateUrl: './seleccion.component.html',
+  styleUrl: './seleccion.component.css'
+})
+export class SeleccionComponent {
+
+}
